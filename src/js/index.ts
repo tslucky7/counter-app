629cc3527c5562d9007f7a8e309f5a21aad524f6
齊藤拓海 <takumi1222122212221222@gmail.com>
2025-07-15 16:43:54 +0900

    カウントの上限・下限の実装、操作のログの実装

--- src/js/index.ts
+++ src/js/index.ts
@@ -6,6 +6,7 @@ const countDisplay = document.getElementById("counter");
 let intCountDisplay = parseInt(countDisplay?.textContent || "0");
 const reset = document.getElementById("reset");
 const stepInput = document.getElementById("step") as HTMLInputElement | null;
+const logList = document.getElementById("log-list");
 
 // ステップ数に応じて入力ボタンのテキストを変更
 if (decrement) {
@@ -26,16 +27,37 @@ stepInput?.addEventListener("input", () => {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
+  // カウントアップボタン
   increment?.addEventListener("click", () => {
-    intCountDisplay += stepInput ? parseInt(stepInput.value) : 1;
+    // カウントの上限
+    if (intCountDisplay < 10) {
+      intCountDisplay += stepInput ? parseInt(stepInput.value) : 1;
+    }
     countDisplay!.textContent = intCountDisplay.toString();
+    // ログを追加
+    const logItem = document.createElement("li");
+    logItem.textContent = `+${stepInput!.value.toString()}しました`
+    logList?.appendChild(logItem);
   });
+  // カウントダウンボタン
   decrement?.addEventListener("click", () => {
-    intCountDisplay -= stepInput ? parseInt(stepInput.value) : 1;
+    // カウントの下限
+    if (intCountDisplay > -10) {
+      intCountDisplay -= stepInput ? parseInt(stepInput.value) : 1;
+    }
     countDisplay!.textContent = intCountDisplay.toString();
+    // ログを追加
+    const logItem = document.createElement("li");
+    logItem.textContent = `-${stepInput!.value.toString()}しました`;
+    logList?.appendChild(logItem);
   });
+  // リセットボタン
   reset?.addEventListener("click", () => {
     intCountDisplay = 0;
     countDisplay!.textContent = intCountDisplay.toString();
+    // ログを追加
+    const logItem = document.createElement("li");
+    logItem.textContent = "リセットしました";
+    logList?.appendChild(logItem);
   })
 });
